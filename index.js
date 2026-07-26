eae2b02211239b4aeaae17c64a9efe420f560776
Mason <mirabito.mason@gmail.com>
2026-07-26 18:38:06 +0930

    add noise to background and centre deliberation photo

--- index.js
+++ index.js
@@ -12,3 +12,13 @@ setInterval(() => {
 
     slides.style.transform = `translatex(-${index * 403}px)`;
 }, 5000);
+
+const background = document.querySelector(".background");
+
+document.addEventListener("mousemove", (e) => {
+  const x = (e.clientX / window.innerWidth - 0.5) * 2;
+  const y = (e.clientY / window.innerHeight - 0.5) * 2;
+
+  background.style.transform =
+    `translate(${x * 15}px, ${y * 15}px)`;
+});
